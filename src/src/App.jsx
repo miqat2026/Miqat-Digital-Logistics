6c1f5b203569a9a03bd7b6725601e12d4bf4c008
miqat2026 <spiderm2017@gmail.com>
2026-05-17 16:59:11 +0300

    Update App.jsx

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,1 +1,31 @@
+export default function App() {
+  return (
+    <div style={{
+      background:"#0A192F",
+      height:"100vh",
+      color:"white",
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"center",
+      flexDirection:"column",
+      fontFamily:"sans-serif"
+    }}>
 
+      <img
+        src="https://i.postimg.cc/tn0TgxJ4/logo-png.png"
+        style={{width:"180px"}}
+      />
+
+      <h1>شركة ميقات اللوجستية الرقمية</h1>
+
+      <h2 style={{color:"#D4AF37"}}>
+        MIQAT DIGITAL LOGISTICS CO
+      </h2>
+
+      <p>
+        فخامة السفر .. بذكاء المستقبل
+      </p>
+
+    </div>
+  )
+}
